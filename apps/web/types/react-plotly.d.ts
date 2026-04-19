b55abb9ffa1bee976deafa5c67722bc57e17473d
basamba1990 <196730336+basamba1990@users.noreply.github.com>
2026-04-19 03:13:32 -0400

    fix: detailed type definition for react-plotly.js to resolve 'data' property error

--- apps/web/types/react-plotly.d.ts
+++ apps/web/types/react-plotly.d.ts
@@ -1,1 +1,22 @@
-declare module 'react-plotly.js';
+declare module 'react-plotly.js' {
+  import { Component } from 'react';
+  import { PlotData, Layout, Config } from 'plotly.js';
+
+  interface PlotParams {
+    data: Partial<PlotData>[];
+    layout?: Partial<Layout>;
+    config?: Partial<Config>;
+    frames?: any[];
+    onInitialized?: (figure: Readonly<any>, graphDiv: Readonly<HTMLElement>) => void;
+    onUpdate?: (figure: Readonly<any>, graphDiv: Readonly<HTMLElement>) => void;
+    onPurge?: (figure: Readonly<any>, graphDiv: Readonly<HTMLElement>) => void;
+    onError?: (err: Readonly<Error>) => void;
+    style?: React.CSSProperties;
+    className?: string;
+    useResizeHandler?: boolean;
+    debug?: boolean;
+    revision?: number;
+  }
+
+  export default class Plot extends Component<PlotParams> {}
+}
